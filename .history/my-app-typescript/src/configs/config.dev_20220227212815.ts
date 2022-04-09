69c912822a00a1f17b42186bb60e32d78a1ae3f5
Rajan <ilikeflex@yahoo.com>
2022-04-09 10:03:02 -0400

    Remove history folder

--- .history/my-app-typescript/src/configs/config.dev_20220227212815.ts
+++
@@ -1,7 +0,0 @@
-import Env from "../components/environments/AppEnvironment.enum";
-import { IEnvfiguration } from "../components/environments/IConfiguration";
-
-export const devConfiguration: IEnvfiguration = {
-    ENVIRONMENT: Env.DEV,
-    ANALYTICS: "https://firebase-dev/env.json"
-}
--- .history/my-app-typescript/src/configs/config.dev_20220227213711.ts
+++
@@ -1,7 +0,0 @@
-import Env from "../components/environments/AppEnvironment.enum";
-import { IEnvConfig } from "../components/environments/IConfiguration";
-
-export const devConfiguration: IEnvConfig = {
-    ENVIRONMENT: Env.DEV,
-    ANALYTICS: "https://firebase-dev/env.json"
-}
